b2b953dcd1b9e69d38db282fa41072439f564282
Neelam <meritneelam2004@gmail.com>
2025-06-18 13:08:12 +0000

    Created function for the working of the calculator
    Created multiple eventlisteners and functions to add the functionality to the calculator
    
    - Add the eventlistener to get the input and populate the input field
    It also identifies the type of input we are getting, whether it is an operator or operand
    
    -Created the function handleOperator that calculates each expression before allowing the user to enter next operands
    
    -Then we have the equalButton that will finalize the result and set all the operands to null again
    
    -The clear button's working is also applied so that it resets the calculator whenever the button is clicked

--- calculator.js
+++ calculator.js
@@ -12,6 +12,7 @@ function multiply(a,b){
 }
 
 function divide(a,b){
+    if(b==0) return "You can't divide by 0!";
     return a/b;
 }
 
@@ -31,9 +32,83 @@ function operate(num1, num2, op){
             return divide(num1, num2);
 
         default:
-            return "Enter a valid operator";
+            return num2;
     }
 }
 
 
+let firstOperand = null;
+let operator = null;
+let secondOperand = false;
+
+
+const inputField = document.querySelector('#inputField input')
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        const value = button.textContent;
+
+        if(['+', '-', '*', '/'].includes(value)){
+            handleOperator(value);
+            return;
+        }
+
+        if(secondOperand){
+            inputField.value = value;
+            secondOperand = false;
+        }
+        else{
+            inputField.value = inputField.value === '0'? value : inputField.value + value;
+        }
+    });
+});
+
+function handleOperator(nextOperator){
+    const inputValue = parseFloat(inputField.value);
+
+    if(operator && secondOperand){
+        operator = nextOperator;
+        return;
+    }
+
+    if(firstOperand === null){
+        firstOperand = inputValue;
+    }else if(operator){
+        const result = operate(firstOperand, inputValue, operator);
+        inputField.value = (typeof result === "number" && result%1 !==0)? result.toFixed(6) : result;
+        firstOperand = parseFloat(inputField.value);
+    }
+
+    operator = nextOperator;
+    secondOperand = true;
+}
+
+
+
+const equalButton = document.querySelector('#equal');
+
+equalButton.addEventListener('click', ()=>{
+    const inputValue = parseFloat(inputField.value);
+
+    if(operator === null || secondOperand){
+        return;
+    }
+
+    const result = operate(firstOperand, inputValue, operator);
+    inputField.value = (typeof result === "number" && result % 1 !== 0)? result.toFixed(6):result;
+    firstOperand = null;
+    operator = null;
+    secondOperand = false;
+});
+
+
+const clearButton = document.querySelector('#clear');
+
+clearButton.addEventListener('click', ()=>{
+    inputField.value = "";
+    firstOperand = null;
+    operator = null;
+    secondOperand = false;
+});
 
